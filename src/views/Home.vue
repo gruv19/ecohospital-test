f8b4543f0b633ecaf981fb32f67e70cbf27baf93
gruv19 <grv1992@yandex.ru>
2022-07-28 22:45:14 +0500

    feat: Добавил в компонент функции вывода списка документов и загрузки документа

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <main>
-    <a href="#" @click.prevent="logout">Выйти</a>
+    <div>
+      <a href="#" @click.prevent="logout">Выйти</a>
+    </div>
+    <ul v-if="documents.length">
+      Документы
+      <li v-for="doc in documents" :key="doc.id_document">
+        {{ doc.doc_name }}
+        <a 
+          href="#" 
+          @click.prevent="download(doc.id_document, doc.doc_type)" 
+        >
+          Скачать
+        </a>
+      </li>
+    </ul>
   </main>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      documents: [],
+    };
+  },
   methods: {
     logout() {
       this.$store.commit('setIdLogin', '');
       this.$store.commit('setTk', '');
       this.$router.push('/login');
+    },
+    async download(id, type) {
+      const document = await this.$store.dispatch('getDocument', { id, type });
+      const tempLink = window.document.createElement('a');
+      tempLink.href = `https://host1.medsafe.tech:40443/${document.hash}`;
+      tempLink.setAttribute('download', '');
+      tempLink.click();
+      tempLink.remove();
     }
+  },
+  async mounted() {
+    this.documents = await this.$store.dispatch('getDocumentList');
   },
 }
 </script>
