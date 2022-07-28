4afdd5a92f66a558add256846d9157032679d14b
gruv19 <grv1992@yandex.ru>
2022-07-28 19:30:30 +0500

    feat: Добавил генерацию fingerPrint-a

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,24 @@
 import { createStore } from 'vuex'
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
 export default createStore({
   state: {
+    imei: '',
   },
   getters: {
   },
   mutations: {
+    setImei(state, imei) {
+      state.imei = imei;
+    }
   },
   actions: {
+    async getFingerPrint({ commit }) {
+      const fpPromise = FingerprintJS.load();
+      const fp = await fpPromise
+      const result = await fp.get();
+      commit('setImei', result.visitorId);
+    }
   },
   modules: {
   }
