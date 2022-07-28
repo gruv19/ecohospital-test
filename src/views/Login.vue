6a8e5b51229ddf73174ae4a3823757bb7359508b
gruv19 <grv1992@yandex.ru>
2022-07-28 19:29:45 +0500

    Инициализировал проект

---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <form action="#" @submit.prevent="login">
+    <input type="text" name="login" placeholder="Логин...">
+    <input type="password" name="password" placeholder="Пароль...">
+    <button type="submit">Войти</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+
+}
+</script>
