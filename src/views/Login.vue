6ebfda1ccd0b007d23067e5b08518a93cfb9ad12
gruv19 <grv1992@yandex.ru>
2022-07-28 21:43:06 +0500

    feat: Реализовал основной функционал компонента

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
-  <form action="#" @submit.prevent="login">
-    <input type="text" name="login" placeholder="Логин...">
-    <input type="password" name="password" placeholder="Пароль...">
+  <form action="#" @submit.prevent="authenticate">
+    <input type="text" name="login" placeholder="Логин..." v-model="login">
+    <input type="password" name="password" placeholder="Пароль..." v-model="password">
     <button type="submit">Войти</button>
   </form>
 </template>
@@ -9,6 +9,24 @@
 <script>
 export default {
   name: 'Login',
-
+  data() {
+    return {
+      login: '',
+      password: '',
+    };
+  },
+  methods: {
+    async authenticate() {
+      const loginResult = await this.$store.dispatch('login', { login: this.login, password: this.password });
+      if (loginResult.id_login) {
+        this.$store.commit('setIdLogin', loginResult.id_login);
+        this.$store.commit('setTk', loginResult.TK);
+        this.$router.push('/');
+        return;
+      } else {
+        alert('Неверный логи или пароль!');
+      }
+    }
+  },
 }
 </script>
