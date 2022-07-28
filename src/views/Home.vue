bb4ca0347692c348a6e9089b017814fc20b4b435
gruv19 <grv1992@yandex.ru>
2022-07-28 21:44:00 +0500

    feat: Создал компонент и добавил функционал разлогина

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <a href="#" @click.prevent="logout">Выйти</a>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    logout() {
+      this.$store.commit('setIdLogin', '');
+      this.$store.commit('setTk', '');
+      this.$router.push('/login');
+    }
+  },
+}
+</script>
